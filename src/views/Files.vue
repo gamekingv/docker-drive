51a2dd3ed3d33d1dc45d075aa9865a47560c1864
gamekingv <793470218@qq.com>
2021-02-06 00:55:55 +0800

    0.1

--- src/views/Files.vue
+++ src/views/Files.vue
@@ -3,13 +3,12 @@
     <v-row>
       <v-col cols="12" sm="3">
         <v-select
-          v-model="activeRepository"
-          :items="repositoryURLs"
+          v-model="activeRepositoryID"
+          :items="repositories"
           item-text="name"
-          item-value="url"
           label="Solo field"
           solo
-          @change="getManifests"
+          @change="switchRepository()"
         ></v-select>
       </v-col>
     </v-row>
@@ -26,6 +25,50 @@
         </v-card-text>
       </v-card>
     </v-dialog>
+    <v-dialog v-model="loginForm" persistent max-width="400px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ $t("needLogin") }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-form ref="form" lazy-validation>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="username"
+                    :label="$t('account')"
+                    :rules="[(v) => !!v || $t('require')]"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="password"
+                    :label="$t('password')"
+                    :rules="[(v) => !!v || $t('require')]"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            @click="$refs.form.validate() && !login() && closeForm()"
+          >
+            {{ $t("ok") }}
+          </v-btn>
+          <v-btn color="error" text @click="closeForm()">
+            {{ $t("cancel") }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-snackbar v-model="alert" :color="alertColor" :timeout="5000" top right>
       {{ alertText }}
     </v-snackbar>
@@ -38,6 +81,20 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue';
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
+interface VForm extends Vue {
+  validate(): boolean;
+  reset(): void;
+  resetValidation(): void;
+}
+
+interface Repository {
+  name: string;
+  url: string;
+  value: symbol;
+  token: string;
+  secret: string;
+}
+
 @Component({
   components: {
     HelloWorld
@@ -45,22 +102,50 @@ import axios, { AxiosInstance, AxiosResponse } from 'axios';
 })
 
 export default class Files extends Vue {
-  private loading = true
+  $refs!: {
+    form: VForm;
+  }
+
+  private loading = false
+  private loginForm = false
+  private beforeLogin!: { fn: string; arg: string[] }
   private result = ''
-  private secret = ''
-  private token = ''
+  private username = ''
+  private password = ''
   private alert = false
   private alertText = ''
   private alertColor = ''
-  private activeRepository = 'registry.cn-hangzhou.aliyuncs.com/kdjvideo/test'
-  private repositoryURLs: { name: string; url: string }[] = [{ name: 'kdjvideo', url: 'registry.cn-hangzhou.aliyuncs.com/kdjvideo/kdjvideo' }, { name: 'test', url: 'registry.cn-hangzhou.aliyuncs.com/kdjvideo/test' }]
-  async mounted(): Promise<void> {
+  private activeRepositoryID!: symbol
+  private activeRepository!: Repository
+  private repositories!: Repository[]
+
+  async created(): Promise<void> {
+    this.repositories = [
+      {
+        name: 'kdjvideo',
+        value: Symbol(),
+        url: 'registry.cn-hangzhou.aliyuncs.com/kdjvideo/kdjvideo',
+        token: '',
+        secret: ''
+      }, {
+        name: 'test',
+        value: Symbol(),
+        url: 'registry.cn-hangzhou.aliyuncs.com/kdjvideo/test',
+        token: '',
+        secret: ''
+      }
+    ];
+    this.activeRepositoryID = this.repositories[1].value;
+    this.activeRepository = this.repositories[1];
     await this.getManifests();
-    this.loading = false;
   }
   private async getManifests(): Promise<void> {
-    const [server, namespace, repository] = this.activeRepository.split('/');
-    if (!server || !namespace || !repository) return this.showAlert(`${this.$t('repositoryFormatError')}`, 'error');
+    this.loading = true;
+    const [server, namespace, repository] = this.activeRepository.url.split('/');
+    if (!server || !namespace || !repository) {
+      this.loading = false;
+      return this.showAlert(`${this.$t('repositoryFormatError')}`, 'error');
+    }
     const manifestsURL = `https://${server}/v2/${namespace}/${repository}/manifests/latest`;
     const manifestsInstance = axios.create({
       method: 'get',
@@ -73,7 +158,7 @@ export default class Files extends Vue {
       const { data } = await this.requestSender(manifestsURL, manifestsInstance);
       const digest: string = data?.config?.digest;
       if (digest) {
-        const configURL = `https://registry.cn-hangzhou.aliyuncs.com/v2/kdjvideo/test/blobs/${digest}`;
+        const configURL = `https://${server}/v2/${namespace}/${repository}/blobs/${digest}`;
         const configInstance = axios.create({
           method: 'get',
           headers: {
@@ -88,60 +173,81 @@ export default class Files extends Vue {
       else this.showAlert(`${this.$t('loadConfigFailed')}`, 'error');
     }
     catch (error) {
-      if (error === -1) this.showAlert('need login');
+      if (error === -1) this.loginPromp('getManifests');
       else if (typeof error === 'string') this.showAlert(error, 'error');
       else this.showAlert(`${this.$t('unknownError')}${error.toString()}`, 'error');
     }
+    this.loading = false;
   }
   private async requestSender(url: string, instance: AxiosInstance): Promise<AxiosResponse> {
     instance.defaults.timeout = 30000;
-    if (this.token) instance.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+    if (this.activeRepository.token) instance.defaults.headers.common['Authorization'] = `Bearer ${this.activeRepository.token}`;
     try {
       return await instance.request({ url });
     }
     catch (error) {
-      const { status, headers } = error.response;
-      if (status === 401) {
-        const token = await this.getToken(headers['www-authenticate']);
-        if (token) {
-          this.token = token;
-          instance.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
-          try {
-            return await instance.request({ url });
+      if (error.response) {
+        const { status, headers } = error.response;
+        if (status === 401) {
+          const token = await this.getToken(headers['www-authenticate']);
+          if (token) {
+            this.activeRepository.token = token;
+            console.log(this.activeRepository);
+            instance.defaults.headers.common['Authorization'] = `Bearer ${this.activeRepository.token}`;
+            try {
+              return await instance.request({ url });
+            }
+            catch (error) {
+              const { status } = error.response;
+              if (status === 401) throw (-1);
+              else throw error;
+            }
           }
-          catch (error) {
-            const { status } = error.response;
-            if (status === 401) throw (-1);
-            else throw error;
-          }
+          else throw (this.$t('getTokenFailed'));
         }
-        else throw (this.$t('getTokenFailed'));
       }
-      else throw error;
+      throw error;
     }
   }
-  // private async login(): Promise<AxiosResponse> {
-  // let status: number = error.response.status, isCancel: boolean = false;
-  // while (status === 401 || !isCancel) {
+  private loginPromp(fn?: string, arg?: string[]): void {
+    this.loginForm = true;
+    this.beforeLogin = { fn: fn ?? '', arg: arg ?? [] };
 
-  //   if
-  // }
-  // }
+  }
+  private login(): void {
+    this.activeRepository.secret = btoa(`${this.username}:${this.password}`);
+    //this.beforeLogin.fn ?? this[this.beforeLogin.fn] ?? this[this.beforeLogin.fn](...this.beforeLogin.arg);
+  }
   private async getToken(authenticateHeader: string): Promise<string | undefined> {
     const [, realm, service, , scope] = authenticateHeader?.match(/^Bearer realm="([^"]*)",service="([^"]*)"(,scope="([^"]*)"|)/) ?? [];
     if (realm && service) {
       let authenticateURL = `${realm}?service=${service}`;
       if (scope) authenticateURL += `&scope=${scope}`;
       const headers: { 'Authorization'?: string } = {};
-      if (this.secret) headers['Authorization'] = `Basic ${this.secret}`;
-      const { data } = await axios.get(authenticateURL, { headers, timeout: 50000 });
+      if (this.activeRepository.secret) headers['Authorization'] = `Basic ${this.activeRepository.secret}`;
+      const { data } = await axios.get(authenticateURL, { headers, timeout: 5000 });
       return data.token;
     }
+  }
+  private switchRepository(): void {
+    this.activeRepository = this.repositories.find(e => e.value === this.activeRepositoryID) ?? {
+      name: '',
+      url: '',
+      value: Symbol(),
+      token: '',
+      secret: ''
+    };
+    this.getManifests();
   }
   private showAlert(text: string, type?: string): void {
     this.alert = true;
     this.alertText = text;
     this.alertColor = type ?? '';
+  }
+  private closeForm(): void {
+    this.$refs.form.reset();
+    this.$refs.form.resetValidation();
+    this.loginForm = false;
   }
 }
 </script>
