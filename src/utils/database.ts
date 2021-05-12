4e1fa391196e0054f8f51b3d8679fc32fc2f0d20
gamekingv <793470218@qq.com>
2021-05-12 20:02:17 +0800

    优化数据库并行操作

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -201,12 +201,30 @@ export default {
       item.uuid = data.uuids[0];
       item._id = `${parent}:${item.uuid}`;
     }
-    const { data } = await client.post(`${repository.databaseURL}/${databaseName}`, item, {
-      headers: {
-        'Content-Type': 'application/json'
+    try {
+      const { data } = await client.post(`${repository.databaseURL}/${databaseName}`, item, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      return data.id.split(':')[1];
+    }
+    catch (error) {
+      if (error.response) {
+        if (error.response.status === 409 && error.response.data?.error === 'conflict') {
+          const { headers } = await client.head(`${repository.databaseURL}/${databaseName}/${item._id}`);
+          if (headers['etag']) item._rev = headers['etag'].replaceAll('"', '');
+          else throw error;
+          const { data } = await client.post(`${repository.databaseURL}/${databaseName}`, item, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+          return data.id.split(':')[1];
+        }
       }
-    });
-    return data.id.split(':')[1];
+      throw error;
+    }
   },
   async rename(item: FileItem, parent: string, repository: Repository): Promise<void> {
     const [databaseItem] = await this.search(item.name, parent, repository);
